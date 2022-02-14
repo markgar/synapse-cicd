72b422d4ab546a3fa933508917a356711abe3f51
Mark Garner <mgarner@microsoft.com>
2022-02-14 11:57:47 -0600

    add role assignment

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,4 @@
 $deploymentName = Get-Date -Format "yyyyMMddHHmmss"
 
 az deployment group create -g synapse-cicd-3 --template-file ./main.bicep --parameters main.parameters.json --name $deploymentName
+az synapse role assignment create --workspace-name mynewworkspace-mg --role "Synapse Administrator" --assignee mgarner@microsoft.com
