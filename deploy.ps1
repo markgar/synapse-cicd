89ba6a5c4e003c7ee9e6c89c5d29cff97e73c610
Mark Garner <mgarner@microsoft.com>
2022-02-17 10:12:25 -0600

    simplify

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,6 @@
 $deploymentName = Get-Date -Format "yyyyMMddHHmmss"
+$rgName = "synapse-demo"
+$sqlAdminPassword = ${{ secrets.SYNAPSE_SQL_ADMIN_PASSWORD }}
 
-az group create -n e2e-4 --location eastus
-az deployment group create -g e2e-4 --template-file ./main.bicep --parameters "synapseSqlAdminPassword=!@#123qweasd" --name $deploymentName
+az group create -n $rgName --location eastus
+az deployment group create -g $rgNameo --template-file ./main.bicep --parameters "synapseSqlAdminPassword=$sqlAdminPassword" --name $deploymentName
